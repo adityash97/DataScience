2864b2bf9cd0a039915823a78043d3868eac73a9
adityash97 <adityash212@gmail.com>
2024-09-13 12:06:11 +0530

    API Key Working | chain need to be fixed

--- projects/pdf_chat/app.py
+++ projects/pdf_chat/app.py
@@ -6,7 +6,7 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import openai
+from langchain.llms.openai import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 
@@ -38,21 +38,25 @@ def main():
         )
         chunks = text_splitter.split_text(text=text)
         import os
-        if os.path.exists(f"{file_name}.pkl"):
-            with open(f"{file_name}.pkl",'rb') as f:
-                VectorStore = pickle.loads(f)
-        else:
-            embeddings = OpenAIEmbeddings() 
-            VectorStore = FAISS.from_texts(chunks,embedding = embeddings)
-            file_name = pdf.name[:-4]
-            with open(f"{file_name}.pkl",'wb') as f:
-                pickle.dump(VectorStore,f)
+        file_name = pdf.name[:-4]
+        # if os.path.exists(f"{file_name}.pkl"):
+        #     with open(f"{file_name}.pkl",'rb') as f:
+        #         VectorStore = pickle.loads(f)
+        # else:
+            # import pdb;pdb.set_trace()
+        embeddings = OpenAIEmbeddings() 
+        VectorStore = FAISS.from_texts(chunks,embedding = embeddings)
+        # file_name = pdf.name[:-4]
+        # from joblib import dump
+        # dump(VectorStore, f"{file_name}.pkl")
+            # with open(f"{file_name}.pkl",'wb') as f:
+            #     pickle.dump(VectorStore,f)
         st.write(chunks)
         query = st.text_input("Enter Your Query")
         if query:
             docs = VectorStore.similarity_search(query=query,k=3) # get top matched document chunk
             # feed the chunk to LLM to get the response
-            llm = openai() # model_name= gpt-3.5-turbo 
+            llm = OpenAI() # model_name= gpt-3.5-turbo 
             chain = load_qa_chain(llm=llm,chain_type="stuff")
             with get_openai_callback as cb:
                 response = chain.run(input_documents=docs,question=query)
