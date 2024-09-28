2c7ee7517c815c1180e3a81d3a859f108f1b944e
adityash97 <adityash212@gmail.com>
2024-09-28 08:54:24 +0530

    more modules

--- projects/chat_bot/app.py
+++ projects/chat_bot/app.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-
-
-
-
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+import streamlit as st
 
 load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
