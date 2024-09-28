d0fc3098a352bd0031256357f7d601c0c631e0a8
adityash97 <adityash212@gmail.com>
2024-09-29 04:50:42 +0530

    Formatter

--- projects/chat_bot/app.py
+++ projects/chat_bot/app.py
@@ -18,24 +18,27 @@ os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a highly knowledgeable and friendly assistant, capable of answering questions and solving problems clearly and concisely. Engage with users in a helpful and approachable manner."),
-        ("user","question : {question}")
+        (
+            "system",
+            "You are a highly knowledgeable and friendly assistant, capable of answering questions and solving problems clearly and concisely. Engage with users in a helpful and approachable manner.",
+        ),
+        ("user", "question : {question}"),
     ]
 )
 
 ## Stream Lit implementation
 
 st.title("Chat Bot With Open AI")
-input_text =  st.chat_input("What do you say (Hit Enter after typing..) ?") 
+input_text = st.chat_input("What do you say (Hit Enter after typing..) ?")
 
 ## Open AI LLM
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 output_parser = StrOutputParser()
 
-chains = prompt|llm|output_parser
+chains = prompt | llm | output_parser
 
 ## Invoking Chain
 
 if input_text:
-    st.write(chains.invoke({'question':input_text}))
+    st.write(chains.invoke({"question": input_text}))
