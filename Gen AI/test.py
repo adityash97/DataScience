b141028ca6361db7fbd3cd4040e89e7813ecb67c
Aditya Anand <adityash212@gmail.com>
2026-04-27 05:15:09 +0530

    Mistral api call success

--- Gen AI/test.py
+++ Gen AI/test.py
@@ -1,3 +1,30 @@
-import langchain
+from dotenv import load_dotenv
+import os
+import json
 
-print("version : ",langchain.__version__)
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
+mistral_api_key = os.getenv("MISTRAL_API_KEY")
+hugging_face_api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+
+
+# mistral ai access
+
+from langchain_mistralai import ChatMistralAI
+llm = ChatMistralAI(
+    model="mistral-small-latest",
+    api_key=mistral_api_key,
+    temperature=0.7
+)
+
+
+response = llm.invoke("What is the capital of India?")
+
+print(response)
+content = {'response':response.content}
+
+with open('mistral_response.json','w') as f:
+    json.dump(content, f, indent=4)
+
