37648879f97c1be508cafb081e36a8a223a90c53
adityash97 <adityash212@gmail.com>
2026-05-28 20:17:11 +0530

    auth restructured | jwt addeed | token verified added

--- projects/AI_Orchestrator/AI_Orchestrator/urls.py
+++ projects/AI_Orchestrator/AI_Orchestrator/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', include('health.urls')),
     path('api/workflows/', include('workflows.urls')),
+    path('api/token/', include('orchestrator_auth.urls')),
 ]
